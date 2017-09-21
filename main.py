90309bb7a0b8435238532473e41812ddbdb4132b
Ryan Zajac <ryan4life12@gmail.com>
2017-09-20 20:58:42 -0500

    updated app to return output in textarea instead of in a new page, using str.format

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -30,20 +30,20 @@ form = """
       <form method="post">
           <label for="rot">Rotate</label>
           <input type="text" name="rot" value="0">
-          <textarea name="text" ></textarea>
+          <textarea name="text" >{0}</textarea>
           <input type="submit" value="Submit">
       </form>
     </body>
 </html>"""
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 
 @app.route("/", methods=['POST'])
 def encrypt():
     rot = int(request.form['rot'])
     text = str(request.form['text'])
     enc_str = str(rotate_string(text,rot))
-    return '<h1>' + enc_str + '</h1>'
+    return form.format(enc_str)
     
 app.run()
